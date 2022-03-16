7a272e66154bad3e7d2f80f8895a41884c2db9b7
marconesns <marcones.ns@gmail.com>
2022-03-16 15:50:35 -0300

    Configuração do main.tf single master

--- main.tf
+++ main.tf
@@ -10,40 +10,10 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
-module "lb_module" {
-  source = "./modules/vm/"
-
-  vm_count = 2
-  vm_hostname_prefix = "lb"
-  vcpu = 1
-  memory = "512"
-  pool = "hd"
-  system_volume = 20
-  
-  dhcp = false
-  ip_address = [
-                "192.168.122.101",
-                "192.168.122.102"
-                ]
-  ip_gateway = "192.168.122.1"
-  ip_nameserver = "192.168.122.1"
-
-  local_admin = "local-admin"
-  ssh_admin = "root"
-  local_admin_passwd = "centos"
-  ssh_private_key = "~/.ssh/id_rsa"
-  ssh_keys    = [ 
-    "ssh-rsa AAAAB3N", 
-    ]
-  
-  time_zone   = "UTC"
-  os_img_url  = "/home/marcones/dados/Isos/centos7-disk001.qcow2"
-}
-
 module "k8smaster_module" {
   source = "./modules/vm/"
 
-  vm_count = 3
+  vm_count = 1
   vm_hostname_prefix = "k8smaster"
   vcpu = 2
   memory = "2048"
@@ -51,11 +21,7 @@ module "k8smaster_module" {
   system_volume = 20
   
   dhcp = false
-  ip_address = [
-                "192.168.122.110",
-                "192.168.122.111",
-                "192.168.122.112"
-                ]
+  ip_address = ["192.168.122.100"]
   ip_gateway = "192.168.122.1"
   ip_nameserver = "192.168.122.1"
 
@@ -64,7 +30,7 @@ module "k8smaster_module" {
   local_admin_passwd = "centos"
   ssh_private_key = "~/.ssh/id_rsa"
   ssh_keys    = [ 
-    "ssh-rsa AAAAB3N", 
+    "ssh-rsa AAAAB3NzaC1yc", 
     ]
   
   time_zone   = "UTC"
@@ -74,7 +40,7 @@ module "k8smaster_module" {
 module "k8sworker_module" {
   source = "./modules/vm/"
 
-  vm_count = 1
+  vm_count = 2
   vm_hostname_prefix = "k8sworker"
   vcpu = 2
   memory = "2048"
@@ -82,7 +48,8 @@ module "k8sworker_module" {
   system_volume = 20  
   
   ip_address = [
-                "192.168.122.201"
+                "192.168.122.101",
+                "192.168.122.102"
                 ]
   ip_gateway = "192.168.122.1"
   ip_nameserver = "192.168.122.1"
@@ -92,7 +59,7 @@ module "k8sworker_module" {
   local_admin_passwd = "centos"  
   ssh_private_key = "~/.ssh/id_rsa"
   ssh_keys    = [ 
-    "ssh-rsa AAAAB3N", 
+    "ssh-rsa AAAAB3NzaC1yc", 
     ]
   
   time_zone   = "UTC"
